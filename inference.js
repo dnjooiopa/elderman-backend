fa8903a4ef2c9bec3b5af49616b4689265e2aa56
dnjooiopa <dnjooiopa@gmail.com>
2020-01-09 19:13:13 +0700

    first deploy

--- inference.js
+++ inference.js
@@ -4,7 +4,7 @@ async function predict(data) {
     const model = await tf.loadLayersModel("file://./mymodel/model.json");
     const ytensor = model.predict(tf.tensor(data)).flatten();
     const ypred = await ytensor.array();
-    console.log(ypred);
+    return ypred;
 }
 
-predict([[77, 77, 80, 74]]);
+module.exports = predict;
--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const getFirebase = require("./train");
+
+app.use(bodyParser.json());
+
+const predict = require("./inference");
 
 app.get("/", function(req, res) {
     res.send("<h1>Hello</h1>");
 });
 
 app.get("/train", function(req, res) {
-    getFirebase();
     res.send("<h1>train</h1>");
+});
+
+app.post("/predict", async function(req, res) {
+    const data = req.body.data;
+    const ypred = await predict(data);
+    const imax = ypred.indexOf(Math.max(...ypred))+1 // position of team;
+    res.json(imax);
 });
 
 app.listen(8888, function() {
